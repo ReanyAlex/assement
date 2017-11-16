9b8aae105ccead1d17af7cf11a252fce139fdeb5
Alex Reany <reanyalex@gmail.com>
2017-11-16 12:56:18 -0500

    image path issued fixed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,9 +15,10 @@ function flip() {
 function newImg() {
   //grab information from the DOM
   var element = document.querySelector('#change');
+  var pathSplit = element.src.split('/');
 
-  // Tenary expression to check which src to and insert the correct srx
-  element.src === 'http://localhost:3000/assets/images/computer2.jpg'
+  // Tenary expression to check which src to and insert the correct src
+  pathSplit[pathSplit.length - 1] === 'computer2.jpg'
     ? (element.src = 'assets/images/computer2b.jpg')
     : (element.src = 'assets/images/computer2.jpg');
 }
